0732a5e0f598ebdc6bbc8d7be16038b38b1a1ff5
nymer <eri.noyama67@gmail.com>
2022-05-27 18:25:49 +0900

    ABOUT ME 作り込み

--- js/script.js
+++ js/script.js
@@ -1,9 +1,104 @@
+//--------------------------------------------------------
+//Loading画面
+//--------------------------------------------------------
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1800,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#020E34',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#020E34',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
 
-/* セクションHOME */
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+  $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+  $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+  $("#splash").fadeOut();//#splashエリアをフェードアウト
+});
+
+//--------------------------------------------------------
+// ページ共通_文字の動き（スクロール読み込みをトリガーに）
+//--------------------------------------------------------
+// 指定タグに対してクラス名fadeUpTriggerを追加
+$(window).on('load', function(){
+	var arrTag_AddClass_fadeUpTrigger = ['h2','h3','p','img']//fadeUpTriggerクラスを入れたいタグを指定
+	for (var i = 0; i < arrTag_AddClass_fadeUpTrigger.length; i++) {
+		var elm = document.getElementsByTagName(arrTag_AddClass_fadeUpTrigger[i]);
+		
+		for (var j = 0; j < elm.length; j++) {
+			var elm_sub = elm[j];
+			elm_sub.classList.add('fadeUpTrigger');//fadeUpTriggerというクラス名を追記
+		}
+	}
+});
+// 指定クラスに対してクラス名fadeUpTriggerを追加
+$(window).on('load', function(){
+	var arrClass_AddClass_fadeUpTrigger = ['skill-item']//fadeUpTriggerクラスを入れたいクラスを指定
+	for (var i = 0; i < arrClass_AddClass_fadeUpTrigger.length; i++) {
+		var elm = document.getElementsByClassName(arrClass_AddClass_fadeUpTrigger[i]);
+		
+		for (var j = 0; j < elm.length; j++) {
+			var elm_sub = elm[j];
+			elm_sub.classList.add('fadeUpTrigger');//fadeUpTriggerというクラス名を追記
+		}
+	}
+});
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+// 画面をスクロールしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    fadeAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+// =================
+// セクションHOME
+// =================
+
+// 背景の星がキラキラ
 particlesJS("particles-js", {
 	"particles":{
 		"number":{
-			"value":200,//この数値を変更すると星の数が増減できる
+			"value":250,//この数値を変更すると星の数が増減できる
 			"density":{
 				"enable":true,
 				"value_area":800
@@ -29,7 +124,7 @@ particlesJS("particles-js", {
 			}
 		},
 		"size":{
-			"value":1.2,//星の大きさ（最大値）
+			"value":1.4,//星の大きさ（最大値）
 			"random":true,
 			"anim":{
 				"enable":false,
@@ -70,3 +165,55 @@ particlesJS("particles-js", {
 	},
 	"retina_detect":true
 });
+
+
+
+// TextTypingというクラス名がついている子要素（span）を表示から非表示にする定義
+function TextTypingAnime() {
+	$('.TextTyping').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		var thisChild = "";
+		if (scroll >= elemPos - windowHeight) {
+			thisChild = $(this).children(); //spanタグを取得
+			//spanタグの要素の１つ１つ処理を追加
+			thisChild.each(function (i) {
+				var time = 100;
+				//時差で表示する為にdelayを指定しその時間後にfadeInで表示させる
+				$(this).delay(time * i).fadeIn(time);
+			});
+		} else {
+			thisChild = $(this).children();
+			thisChild.each(function () {
+				$(this).stop(); //delay処理を止める
+				$(this).css("display", "none"); //spanタグ非表示
+			});
+		}
+	});
+}
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".TextTyping");
+	element.each(function () {
+		var text = $(this).html();
+		var textbox = "";
+		text.split('').forEach(function (t) {
+			if (t !== " ") {
+				textbox += '<span>' + t + '</span>';
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+
+	});
+
+	TextTypingAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
